34bdcbddfcb269d7eda6eac356ace2079510148d
Nichole <nicholewilburn@gmail.com>
2023-06-12 15:12:29 -0500

    current day data is displaying

--- script.js
+++ script.js
@@ -12,6 +12,12 @@ var cityContainer = document.getElementById("city-container");
 var forecastContainer = document.getElementById("forecast-container");
 var fivedayContainer = document.getElementById("fiveday-container");
 
+var cityNameEl = document.getElementById("city-name");
+var todayDateEl = document.getElementById("today-date");
+var todayTemperatureEl = document.getElementById("today-temperature");
+var todayWindEl = document.getElementById("today-wind");
+var todayHumidEl = document.getElementById("today-humid");
+
 //event listeners
 searchBtn.addEventListener('click', grabSearch);
 
@@ -44,7 +50,7 @@ function grabSearch () {
 
     //api for data call
     function getDataAPI() {
-    var requestURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey;
+    var requestURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey + "&units=imperial";
 
     fetch(requestURL)
         .then(  function(reponse) {
@@ -53,6 +59,22 @@ function grabSearch () {
         .then(  function(data) {
             console.log("LOCATION DATA");
             console.log(data);
+
+            var today = dayjs().format("(MM/DD/YYYY)");
+
+            cityNameEl.textContent = data.city.name;
+            todayDateEl.textContent = today;
+            todayTemperatureEl.textContent = "Temperature: " + data.list[0].main.temp;
+            todayWindEl.textContent = "Wind: " + data.list[0].wind.speed;
+            todayHumidEl.textContent = "Humidity: " + data.list[0].main.humidity;
+        
+            //lets dynamically create elements for each day
+            for (let i = 0; i < 5; i++) {
+
+                //day 1
+                
+                
+            }
         })
     } //end getDataAPI
 
